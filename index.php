9af716437df4d0d7f89d7c17b0539e1b67910e34
Bert Smelt <bertsmelt@kpnmail.nl>
2019-05-25 23:24:21 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
-<?php
-  echo "hello world!";
-?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="robots" content="follow">
+
+    <title>Page Title</title>
+
+    <base href="/" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.1.5/css/uikit.min.css" />
+  </head>
+
+  <body>
+    <!--JQuery is required-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <!--UIkit JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.1.5/js/uikit.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.1.5/js/uikit-icons.min.js"></script>
+  </body>
+</html>
